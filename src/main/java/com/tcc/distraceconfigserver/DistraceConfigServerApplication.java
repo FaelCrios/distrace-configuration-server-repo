8b666c9a18933cabae1ec35b87aea6ffff11faa3
Rafael <faelnek@gmail.com>
2024-07-11 10:49:08 -0300

    Finalização do projeto

---
+++ src/main/java/com/tcc/distraceconfigserver/DistraceConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.tcc.distraceconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DistraceConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DistraceConfigServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tcc/distraceconfigserver/DistraceConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tcc.distraceconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DistraceConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
